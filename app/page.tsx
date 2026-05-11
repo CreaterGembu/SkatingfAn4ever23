f21e78ce035172a2f805769210d0a84bc35ca41d
CreaterGembu <s241468r@st.go.tuat.ac.jp>
2026-05-11 13:01:02 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1381,10 +1381,10 @@ const [recentElements, setRecentElements] = useState<Element[]>([]);
           borderRadius: 10,
         }}
       >
-        <div> Total Technical Element Score (TES - Ded): {totalTES.toFixed(2)}</div>
-        <div>PCS applied: {pcsApplied.toFixed(2)}</div>
+        <div> Total Technical Element Score (TES): {totalTES.toFixed(2)}</div>
+        <div>Program Conponent Score (PCS) : {pcsApplied.toFixed(2)}</div>
         <div style={{ fontWeight: 800, marginTop: 6 }}>
-          TOtal: {grandTotal.toFixed(2)}
+          Total Score: {grandTotal.toFixed(2)}
         </div>
       </div>
 
