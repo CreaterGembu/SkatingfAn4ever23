05e0c45e03280a8fca5b900c1286dd7d846a039f
CreaterGembu <s241468r@st.go.tuat.ac.jp>
2026-06-09 09:28:05 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -715,6 +715,7 @@ ${showProtocol.protocolHtml}
       ? 'auto'
       : '1 / -1',
 }}
+          />
         <label style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
           <div style={{ minWidth: 120 }}> Segment </div>
           <select
