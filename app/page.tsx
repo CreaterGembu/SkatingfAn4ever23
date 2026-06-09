b497d20ff9905182fe675b5a98107deb6256c7cb
CreaterGembu <s241468r@st.go.tuat.ac.jp>
2026-06-09 11:33:24 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2203,7 +2203,7 @@ const goeMark =
   <span
     style="
       display:inline-block;
-      width:60px;
+      width:38px;
       text-align:right;
     "
   >
@@ -2409,7 +2409,7 @@ ${rowsHtml}
   <span
     style="
       display:inline-block;
-      width:60px;
+      width:38px;
       text-align:right;
     "
   >
